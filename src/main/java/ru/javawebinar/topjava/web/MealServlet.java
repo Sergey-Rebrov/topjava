a5af4f2c9cc720a11a38765bec75ade3ed6bc69d
Sergey-Rebrov <sergey.rebrov@gmail.com>
2016-12-09 23:08:27 +0700

    HW0

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,27 @@
+package ru.javawebinar.topjava.web;
+
+
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by Sergey on 09.12.2016.
+ */
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to meals");
+
+//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        response.sendRedirect("meals.jsp");
+    }
+}
