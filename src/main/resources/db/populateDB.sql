1e9ac459b2011d6366497bed7829f4abab6c73ce
Sergey-Rebrov <sergey.rebrov@gmail.com>
2016-12-25 19:07:42 +0700

    HW0

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -11,3 +12,8 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories, user_id) VALUES
+  ('Завтрак', 500, 100000),
+  ('Обед', 1000, 100000),
+  ('Ужин', 100, 100001);
