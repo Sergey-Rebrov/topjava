7af081771beb42012d8f0698e927fe587e8cf719
Sergey-Rebrov <sergey.rebrov@gmail.com>
2016-12-27 14:26:14 +0700

    HW0

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -12,6 +12,8 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+import static ru.javawebinar.topjava.UserTestData.*;
+
 /**
  * User: gkislin
  * Date: 22.08.2014
@@ -22,15 +24,15 @@ public class SpringMain {
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "classpath:spring/spring-db.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(UserTestData.USER);
+            adminUserController.update(UserTestData.USER, USER_ID);
             System.out.println();
 
-            /*MealRestController mealController = appCtx.getBean(MealRestController.class);
+            MealRestController mealController = appCtx.getBean(MealRestController.class);
             List<MealWithExceed> filteredMealsWithExceeded =
                     mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExceeded.forEach(System.out::println);*/
+                            LocalDate.of(2016, 12, 1), LocalTime.of(7, 0),
+                            LocalDate.of(2016, 12, 31), LocalTime.of(11, 0));
+            filteredMealsWithExceeded.forEach(System.out::println);
         }
     }
 }
