91da424b2356ebf02781ac3e8cc413eb21b56333
Sergey-Rebrov <sergey.rebrov@gmail.com>
2017-01-17 21:01:32 +0700

    spring_cache

---
+++ src/test/java/ru/javawebinar/topjava/service/ServiceTest.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.topjava.service;
+
+
+/**
+ * Created by Sergey on 17.01.2017.
+ */
+public abstract class ServiceTest {
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/jdbcUserServiceTest.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.service.jdbc;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.service.UserServiceTest;
+
+/**
+ * Created by Sergey on 17.01.2017.
+ */
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles({Profiles.ACTIVE_DB, Profiles.JDBC})
+public class jdbcUserServiceTest extends UserServiceTest {
+}
